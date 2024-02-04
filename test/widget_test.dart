8e57dd8b51979fbaed625fc526e76f7829732673
Were Samson Bruno <werefast2000@gmail.com>
2024-02-04 03:36:56 +0000

    refactored name in test widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:simple_toast/main.dart';
+import 'package:simple_toast_message/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
